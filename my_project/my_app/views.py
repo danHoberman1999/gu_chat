668664872340196e86116fb24d6c5950d0c456fc
Daniel Hoberman <dhoberman@zagmail.gonzaga.edu>
2022-01-26 02:18:51 -0800

    totally redesigned look of website

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "chatroom.html")
+
+def about_page(request):
+    return render(request, "about.html")
+
+def contact_page(request):
+    return render(request, "contact.html")
